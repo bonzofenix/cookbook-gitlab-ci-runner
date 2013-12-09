4687e176bcfcaca9a3579a283967bb79d44eb1e9
bonzofenix <bonzofenix@gmail.com>
2013-12-09 18:40:34 -0300

    adds sudo user for gitlab_ci_runner

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-%w{ apt git build-essential openssl rbenv }.each do |dep|
+%w{ apt git build-essential sudo openssl rbenv }.each do |dep|
   depends dep
 end
