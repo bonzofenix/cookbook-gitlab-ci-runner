57833f04e82298f5d334c87871373d8073cf281f
Alan Moran <bonzofenix@gmail.com>
2014-04-23 10:36:45 -0700

    Modifed git url

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default['gitlab_ci_runner']['gitlab_ci_token'] = 'GITLAB_CI_TOKEN'
 default['gitlab_ci_runner']['ruby']['version'] = '2.0.0-p247'
 default['gitlab_ci_runner']['git']['url'] = '2.0.0-p247'
 default['gitlab_ci_runner']['git']['branch'] = '4-0-stable'
-default['gitlab_ci_runner']['git']['url'] = "git://github.com/gitlabhq/gitlab-ci-runner.git"
+default['gitlab_ci_runner']['git']['url'] = "https://github.com/gitlabhq/gitlab-ci-runner.git"
 
 
 
